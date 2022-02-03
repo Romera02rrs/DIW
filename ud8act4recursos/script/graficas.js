5654800babac34b700f95cfd48a1667baf7586da
Romera02rrs <Romera02rrs@gmail.com>
2022-02-03 11:25:06 +0100

    commit

--- ud8act4recursos/script/graficas.js
+++ ud8act4recursos/script/graficas.js
@@ -1,24 +1,21 @@
 window.onload = main
 
 function main(){
-    new Chart(document.getElementById("bar-chart"), {
-        type: 'bar',
+    new Chart(document.getElementById("pie-chart"), {
+        type: 'pie',
         data: {
-        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-        datasets: [
-            {
+          labels: ["Àfrica", "Asia", "Amèrica"],
+          datasets: [{
             label: "Population (millions)",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-            data: [1000,5267,734,784,433]
-            }
-        ]
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+            data: [2478,5267,734]
+          }]
         },
         options: {
-        legend: { display: false },
-        title: {
+          title: {
             display: true,
             text: 'Predicted world population (millions) in 2050'
-        }
+          }
         }
     });
 }
